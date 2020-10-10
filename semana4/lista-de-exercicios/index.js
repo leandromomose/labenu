dad29a9036288a73823357191ec5911eb0dbfa6f
Leandro-Momose <leandro.momose@gmail.com>
2020-10-10 20:19:11 -0300

    Exercicios de lógica de programação feitos

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -12,3 +12,79 @@
 //4.
 // Existe uma variável chamada numeros, que é uma array com vários elementos (números) e existem outras duas variáveis, numero1 que tem como valor infinito e numero2 que tem valor 0. O primeiro loop diz que para cada elemento do array numeros, se este elemento for menor que o valor da variável numero1, essa variável assume o valor do elemento. O laço percorre toda a array fazendo a comparação e o valor da variável assumida será então o menor valor encontrado dentro dessa array. Então um console.log para a variável numero1 será: -10. O segundo if diz que para cada elemento do array, se o elemento for maior que a variável numero2, começando com o valor dela sendo 0, essa variável assume o valor do elemento. Então, percorrendo tada a array, a variável sempre assumirá o maior valor do elemento dentro da array. Dando um console.log na variável numero2, o valor impresso será: 1590
 
+// Exercícios de lógica de programação
+
+// 1.
+// Para percorrer/iterar uma lista, podemos usar o loop, e 3 maneiras de se fazer isso é utulizando: - while, - for, - for of. 
+//  const numeros = [1, 16, 29,45]
+
+// let i = 0
+// while (i < 4){
+//    console.log(i)
+//    i++
+// }
+
+// for (let i = 0; i < 4; i++){
+//   const elemento = numeros[i]
+//   console.log(elemento)
+// }
+
+// for (let numero of numeros){
+//     console.log(numero)
+// }
+
+// 2. 
+// a. false
+// b. true
+// c. true
+// d. true
+// e. true
+
+// 3. 
+// Este código não funciona pois não existe um incremento para o while e a variável não possui nenhum parâmetro.
+
+// let quantidadeDeNumerosPares = (nPrimeirosNumerosPares) => {
+//     let i = 0
+//     while(i < nPrimeirosNumerosPares) {
+//     console.log(i*2)
+//     i++
+//     }
+// }
+// quantidadeDeNumerosPares(5)
+
+// 4.
+// let trianguloTipo = (a, b, c) =>{
+//     if (a === b && b === c){
+//         console.log("O triângulo é Equilátero")
+//     } else if (a === b && b !== c){
+//         console.log("O triângulo é Isósceles")
+//     } else if (a !== b && b !== c){
+//         console.log("O triângulo é Escaleno")
+//     }
+// }
+
+// trianguloTipo(5, 8, 10)
+
+// 5. 
+// let doisNumeros = (a, b) => {
+//     if (a > b){
+//         console.log(`O maior número é ${a}`)
+//     } else if (a < b){
+//         console.log(`O maior número é ${b}`)
+//     }
+
+//     if (a % b !== 0 && b % a === 0 ){
+//         console.log(`${a} não é divisível por ${b} `)
+//         console.log(`${b} é divisível por ${a} `)
+//     } else if (a % b === 0 || b % a !== 0){
+//         console.log(`${a} é divisível por ${b}`)
+//         console.log(`${b} não é divisível por ${a}`)
+//     }
+
+//     if (a > b){
+//         console.log(`A diferença entre eles é ${a-b}`)
+//     } else if (a < b){
+//         console.log(`A diferença entre eles é ${b-a}`)
+//     }
+// }
+// doisNumeros(15, 30)
