7d43cd5959e06a0ae63b6b529458eb52d8b97e76
Leandro-Momose <leandro.momose@gmail.com>
2020-10-19 17:00:00 -0300

    Atividade Labenu Forms

---
+++ semana6/labenu-forms/labeform/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Etapa1 from './components/Etapa1';
+import Etapa2 from './components/Etapa2';
+import Etapa3 from './components/Etapa3'
+import Final from './components/Final'
+
+export default class App extends React.Component {
+
+  state = {
+    etapa: 1,
+  }
+
+  renderizaEtapa = () => {
+    switch (this.state.etapa){
+      case 1:
+        return <Etapa1 />;
+      case 2:
+        return <Etapa2 />;
+      case 3:
+        return <Etapa3 />;
+      case 4:
+        return <Final />
+    }
+  }
+
+  irParaProximaEtapa = () => {
+    this.setState ({etapa: this.state.etapa + 1})
+  }
+
+  render() {
+    return(
+      <div className="App">
+        {this.renderizaEtapa()}
+        <button onClick={this.irParaProximaEtapa}>Próxima etapa</button>
+      </div>
+    )
+  }
+}
+
+ 
---
+++ semana6/labenu-forms/labeform/src/components/Final.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Final() {
+    return (
+      <div className="Final">
+        <h2>O FORMULÁRIO ACABOU</h2>
+        <p>Muito obrigado por participar! Entraremos em contato!</p>
+      </div>
+    );
+  }
+  
+  export default Final;
