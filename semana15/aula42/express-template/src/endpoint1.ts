c83ec6150a1d274b397369bf8fd8f0e12525a333
Leandro-Momose <leandro.momose@gmail.com>
2021-01-06 22:36:39 -0300

    Construindo endpoints

---
+++ semana15/aula42/express-template/src/endpoint1.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import {countries, country} from './countries'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/countries/all", (req: Request, res: Response) => {
+
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+      }))
+    res.status(200).send(result)
+})
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+})
+
